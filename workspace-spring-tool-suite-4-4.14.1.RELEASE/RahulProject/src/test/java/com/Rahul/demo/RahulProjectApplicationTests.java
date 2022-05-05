1886ec88d626ae1ed9606d300ca7b82a20253b09
unknown <rahulac711@gmail.com>
2022-05-05 17:11:20 +0530

    Spring boot Webapplication

---
+++ workspace-spring-tool-suite-4-4.14.1.RELEASE/RahulProject/src/test/java/com/Rahul/demo/RahulProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Rahul.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RahulProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
